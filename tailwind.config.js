911a6c22ad56a97d0e6dc1a25f0db23e1d220534
webKanchan1234 <kanchankr15153795@gmail.com>
2025-08-04 12:02:24 +0530

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+});
